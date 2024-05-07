19aaf148075aa9c60ca56556ba9b9e6c8c8f2f78
Steven Gestl <105674896+StevenGestl@users.noreply.github.com>
2024-05-07 05:56:26 -0400

    Create workouts.js

---
+++ backend/routes/workouts.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const {
+  getWorkouts, 
+  getWorkout, 
+  createWorkout, 
+  deleteWorkout, 
+  updateWorkout
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
+
+
+// nodemon server.js to run 
+
+// mongo username mario
+//password letsAgo45
+
+// npm install mongoose
